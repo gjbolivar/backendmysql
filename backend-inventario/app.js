b2393cc16057ef1aaad5a02c68091d3a3a029062
Guillermo Bolivar <Gjbolivar@grupocasonita.com>
2025-08-05 10:13:20 -0400

    Fix  sellers

--- backend-inventario/app.js
+++ backend-inventario/app.js
@@ -4,6 +4,7 @@ const app = express();
 const productRoutes = require('./routes/productRoutes');
 const quoteRoutes = require('./routes/quoteRoutes'); 
 const clientRoutes = require('./routes/clientRoutes');
+const sellerRoutes = require('./routes/sellerRoutes');
 
 app.use(cors());
 app.use(express.json());
@@ -15,6 +16,7 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/quotes', quoteRoutes);
 app.use('/api/clients', clientRoutes); 
+app.use('/api/sellers', sellerRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
